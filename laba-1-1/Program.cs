ec4943193174e0aac511034c39cf01c96d79b0b1
shin12123 <bludjust@gmail.com>
2024-03-12 14:17:36 +0200

    Добавьте файлы проекта.

---
+++ laba-1-1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace l_1_1
+{
+    class Program
+    {
+        //Дано текстовий файл.Створіть список, кожен елемент якого містить кількість символів у відповідному рядку тексту.
+        static void Main()
+        {
+
+
+            string filePath = "C:\\txt.txt";
+
+            List<int> lineLengths = GetLineLengths(filePath); //получение списка строк 
+
+            foreach (int length in lineLengths)
+            {
+
+                Console.WriteLine(length);
+
+            }
+            Console.ReadKey();
+        }
+        static List<int> GetLineLengths(string filePath)
+        {
+            List<int> lengths = new List<int>();
+
+            if (File.Exists(filePath))
+            {
+
+
+                string[] lines = File.ReadAllLines(filePath);
+
+
+                foreach (string line in lines)
+                {
+
+                    lengths.Add(line.Length);
+
+                }
+            }
+
+            return lengths;
+
+
+        }
+    }
+}
